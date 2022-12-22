5612dfbaa9f13563e3f1ce1d5db62a20246f388a
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-22 18:13:33 -0300

    commit 2

---
+++ manipulacao.js
@@ -0,0 +1,23 @@
+ const client = {
+    nome: 'pedro',
+    idade: 21,
+    cpf : '12312342341',
+    telefone : ['231233122', '123123432'],
+
+ };
+
+client.endereco = {
+    rua: 'rua 5',
+    numero: '24',
+    apartamento:false,
+    Complemento: 'quadra 4',
+
+}
+
+
+ console.log(client.telefone)
+
+
+console.log(client.endereco.apartamento)
+
+
